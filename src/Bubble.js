f15346fba7a707c18b4c0a700c84ae78aa735d64
hammadj <jutt@ualberta.ca>
2017-07-09 20:07:16 -0600

    Revert "remove minHeight to support rn 0.28.0"
    This reverts commit d3df83a47cf40d993821f303b741181684aed895.

--- src/Bubble.js
+++ src/Bubble.js
@@ -152,6 +152,7 @@ const styles = {
       borderRadius: 15,
       backgroundColor: '#f0f0f0',
       marginRight: 60,
+      minHeight: 20,
       justifyContent: 'flex-end',
     },
     containerToNext: {
@@ -170,6 +171,7 @@ const styles = {
       borderRadius: 15,
       backgroundColor: '#0084ff',
       marginLeft: 60,
+      minHeight: 20,
       justifyContent: 'flex-end',
     },
     containerToNext: {
